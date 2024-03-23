1ddf6d219a1807a0e9ef90d372d6b13379863e3e
Abhinay Yadav <abhinayy48@gmail.com.com>
2024-03-24 01:49:28 +0530

    Final touch

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
